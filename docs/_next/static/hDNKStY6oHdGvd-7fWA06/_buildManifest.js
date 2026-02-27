4002559671c0b305bc88f9a3bdac9a9ed34969b2
Haru1113-cloud <haruya.h@icloud.com>
2026-02-27 21:23:09 +0900

    Export static site for GitHub Pages

---
+++ docs/_next/static/hDNKStY6oHdGvd-7fWA06/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/scriptAI-service//_next/:path+",
+        "destination": "/scriptAI-service/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
